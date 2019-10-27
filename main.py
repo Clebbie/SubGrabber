1597b870ddc97074a6784df5017458ab95d3707f
yarn-ball <cyarnell2036@gmail.com>
2019-10-27 14:28:13 -0500

    Updated index file

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ app = Flask(__name__)
 def hello():
 	return render_template('auth_success.html')
 
+@app.route("/", methods = ['GET'])
+def index():
+	accessToken = 'Test'
+	userID = "Dank"
+	return render_template('index.html', access_token=accessToken, user_id=userID)
+
 @app.route("/auth", methods = ['POST'])
-def getBody():
+def validateKey():
 	data = request.get_json()
 	print(data)
+#run GET request to validate key
+	#return true/ false
 	return 'Success!\n',200
+
+
+
 if __name__ == "__main__":
     app.run()
