2002aa44480ab9456bd5ed0ee3b66629f4041d16
yarn-ball <cyarnell2036@gmail.com>
2019-10-27 15:45:18 -0500

    Updated the main

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import requests
+import json
 app = Flask(__name__)
 
+clientID = 'a0eq55k1fyehqztfwcr0bx6y9b8j5z'
+
+
 @app.route("/auth_success", methods = ['GET'])
-def hello():
+def route_authSuccess():
 	return render_template('auth_success.html')
 
 @app.route("/", methods = ['GET'])
 def index():
-	accessToken = 'Test'
-	userID = "Dank"
-	return render_template('index.html', access_token=accessToken, user_id=userID)
+	accessToken = ''
+	userID = ''
+	isValid = False
+	isFirstTime = True
+	return render_template('index.html', access_token=accessToken, user_id=userID, is_valid=isValid, first_time=isFirstTime)
 
 @app.route("/auth", methods = ['POST'])
-def validateKey():
+def route_auth():
 	data = request.get_json()
-	print(data)
-#run GET request to validate key
+	print(data['access_token'])
+	print(str(validateKey(data['access_token'])))
+	#run GET request to validate key
 	#return true/ false
 	return 'Success!\n',200
+	#return 'Invalid!\n',401
 
 
+def validateKey(key):
+	twitchURL =  'https://id.twitch.tv/oauth2/validate'
+	validationHeaderKey = 'Authorization'
+	validationHeaderValue = 'OAuth ' + key
+	response = requests.get(twitchURL,params=[(validationHeaderKey,validationHeaderValue)])
+
+	data = json.loads(response.content.decode('utf-8'))
+
+	print(data)
+	if(int(data['expires_in']) <= 60):
+		return False
+	return True
 
 if __name__ == "__main__":
     app.run()
