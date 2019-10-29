8bcfddb290480ef53ab14cef36d114145f6cd2fc
yarn-ball <cyarnell2036@gmail.com>
2019-10-29 11:16:25 -0500

    Pushed most of the code to get everything working. Needs to be cleaned

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from appdirs import *
-import os.path
+import os
 import requests
 import json
 app = Flask(__name__)
 
 clientID = 'a0eq55k1fyehqztfwcr0bx6y9b8j5z'
 appDirectory = AppDirs('SubGrabber', 'Yarnball')
-
 
 @app.route("/auth_success", methods = ['GET'])
 def route_authSuccess():
@@ -19,12 +18,19 @@ def route_authSuccess():
 def index():
 	accessToken = ''
 	userID = ''
-	isFirstTime = doesDataExist()
+	isFirstTime = not doesDataExist()
 	if(not isFirstTime):
-		file = open(appDirectory.user_data_dir + '/user.sg')
-		userInfo = json.loads(file.readlines())
-		userID = userInfo['id']
+		file = open(appDirectory.user_data_dir + '/user.sg', 'r')
+		fileData = file.readlines()
+		rawData = ''
+		for line in fileData:
+			rawData += line
+		userInfo = json.loads(rawData)
+		userID = userInfo['data'][0]['id']
+		accessToken = userInfo['data'][0]['access_token']
 	isValid = False
+	print('***************')
+	print('accessToken = ' + accessToken + '\nuserID = ' + userID + '\nisFirstTime= ' + str(isFirstTime))
 	return render_template('index.html', access_token=accessToken, user_id=userID, is_valid=isValid, first_time=isFirstTime)
 
 @app.route("/auth", methods = ['POST'])
@@ -33,6 +39,7 @@ def route_auth():
 	data = request.get_json()
 	print(data['access_token'])
 	if(validateKey(data['access_token'])):
+		createUserFile(data['access_token'])
 		return 'Success!\n',200
 	else:
 		return 'Invalid or Expired key!\n', 401
@@ -58,13 +65,38 @@ def doesDataExist():
 	#Look in user directory
 	if(os.path.exists(appDirectory.user_data_dir)):
 		print("NOT the first time")
+		return True
 	else:
 		print('First time setup')
-	print (dir.user_data_dir)
-	return ""
+		return False
 
+def createUserFile(access_token):
+	if not doesDataExist():
+		os.makedirs(appDirectory.user_data_dir)
+	header = 'Authorization'
+	url = 'https://api.twitch.tv/helix/users'
+	response= requests.get(url,headers={header : 'Bearer ' + access_token})
+	fileName = appDirectory.user_data_dir + '/user.sg'
+	file = open(fileName, 'w+')
+	data = json.loads(response.content)
+	#data['data'][0]['access_token'] = access_token
+	file.write(response.content)
+	file.close()
+	insertAccessToken(access_token)
 
-
+def insertAccessToken(access_token):
+	userInfor = ''
+	file = open(appDirectory.user_data_dir + '/user.sg', 'r')
+	fileData = file.readlines()
+	rawData = ''
+	for line in fileData:
+		rawData += line
+	userInfo = json.loads(rawData)
+	userInfo['data'][0]['access_token'] = access_token
+	fileName = appDirectory.user_data_dir + '/user.sg'
+	file = open(fileName, 'w+')
+	file.write(json.dumps(userInfo,ensure_ascii=True))
+	file.close()
 
 if __name__ == "__main__":
     app.run()
